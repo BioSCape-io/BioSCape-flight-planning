31fd10805214384ffc1206e0d805f64878ce0274
jslingsby <1164222+jslingsby@users.noreply.github.com>
2023-11-28 17:16:40 +0200

    added reverse geocode step for weather watchers

--- code/WeatherWatchers.R
+++ code/WeatherWatchers.R
@@ -4,6 +4,7 @@ library(sf)
 library(tidyverse)
 library(mapview)
 library(stringr)
+library(tidygeocoder)
 
 if (Sys.getenv("USER") == "jasper") {gmail = "jasper.slingsby@uct.ac.za"}
 if (Sys.getenv("USER") == "adamw") {gmail = "adamw@buffalo.edu"}
@@ -21,6 +22,13 @@ boxes <- st_read("data/20231024_G3_AVNG_PRISM_boxes.gpkg")
 folks$Lat <- as.numeric(str_split_i(folks$LatLong, ", ", i = 1))
 folks$Lon <- as.numeric(str_split_i(folks$LatLong, ", ", i = 2))
 
+# Extract addresses
+reverse <- folks %>%
+  reverse_geocode(lat = Lat, long = Lon, method = 'arcgis',
+                  address = address_found, full_results = TRUE) %>%
+  write_sheet(ss = "https://docs.google.com/spreadsheets/d/1blTwiT-67q8vdElK8wdubm_edto2vx7smxwlAmmkdOs/edit?resourcekey#gid=1332651624", sheet = "with_addresses")
+
+
 # Make sf object
 folks <- folks %>% filter(!is.na(Lat)) %>% 
   st_as_sf(coords = c("Lon", "Lat"), crs = 4326)
@@ -30,4 +38,6 @@ boxes <- boxes %>% st_transform(st_crs(folks))
 # Map
 folks %>% select(Name, Number) %>% mapview(label = "Number", color = "magenta", col.regions = "white") +
   mapview(boxes, label = "box_nr")
+
+
 
